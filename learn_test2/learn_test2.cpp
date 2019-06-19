9838f80fb4dabb7b4d5634d3b684a7709320770a
WEIZHONGPING\weizhongping <1335658675@qq.com>
2019-06-19 18:27:33 +0800

    no message

--- learn_test2/learn_test2.cpp
+++ learn_test2/learn_test2.cpp
@@ -3,85 +3,15 @@
 #include <string>
 using namespace std;
 
-/**
- * 定义移动类：Movable
- * 纯虚函数：move
- */
-class Movable
-{
-public:
-	virtual void move() = 0;
-};
-
-/**
- * 定义公交车类：Bus
- * 公有继承移动类
- * 特有方法carry
- */
-class Bus : public Movable
-{
-public:
-	Bus() {};
-
-	virtual void move()
-	{
-		cout << "Bus -- move" << endl;
-	}
-
-	void carry()
-	{
-		cout << "Bus -- carry" << endl;
-	}
-};
-
-/**
- * 定义坦克类：Tank
- * 公有继承移动类
- * 特有方法fire
- */
-class Tank :public Movable
-{
-public:
-	Tank() {};
-
-	virtual void move()
-	{
-		cout << "Tank -- move" << endl;
-	}
-
-	void fire()
-	{
-		cout << "Tank -- fire" << endl;
-	}
-};
-
-/**
- * 定义函数doSomething含参数
- * 使用dynamic_cast转换类型
- */
-void doSomething(Movable* obj)
-{
-	obj->move();
-
-	if (typeid(*obj) == typeid(Bus))
-	{
-		Bus* bus = dynamic_cast<Bus*>(obj);
-		bus->carry();
-	}
-
-	if (typeid(*obj) == typeid(Tank))
-	{
-		Tank* tank = dynamic_cast<Tank*>(obj);
-		tank->fire();
-	}
+template <typename A>
+void dis(A a) {
+	cout << a << endl;
 }
 
 int main(void)
 {
-	Bus b;
-	Tank t;
-	doSomething(&b);
-	doSomething(&t);
+	dis('A');
+	dis(10);
 	return 0;
 }
 
